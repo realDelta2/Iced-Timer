9df6602b8e630cd187c15c33012346051ec53acb
Caleb <realDelta2@outlook.com>
2023-09-14 12:00:42 -0500

    Trying to add messages to pages.rs

--- src/pages.rs
+++ src/pages.rs
@@ -1,5 +1,5 @@
 use iced::Element;
-use iced::widget::{Text};
+use iced::widget::{Text, Button, Column};
 
 use crate::Messages;
 
@@ -15,15 +15,32 @@ impl Selector {
     }
 }
 
-pub struct TimerLive {}
+pub struct TimerLive {
+    timer: i32
+}
+
+#[derive(Debug, Clone, Copy)]
+enum TLMessagse {
+    OnPress
+}
 
 impl TimerLive {
     pub fn new() -> Self {
-        TimerLive {  }
+        TimerLive { timer: 10 }
+    }
+
+    fn update(&mut self, message: TLMessagse) {
+        match message {
+            TLMessagse::OnPress => self.timer += 1
+        }
     }
 
     pub fn view(&self) -> iced::Element<Messages> {
-        Text::from("Chicken nuggets are not good").into()
+        let my_button = Button::new("").on_press(TLMessagse::OnPress);
+        let my_text = Text::new(self.timer.to_string());
+
+        Column::new().push(my_button).push(my_text).into()
+
     } 
 }
 
